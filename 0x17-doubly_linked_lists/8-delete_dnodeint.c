fcde9522ec88330a1313569c4d9edee63edde995
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-18 17:14:59 +0000

    Deletes node at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * Function name:
+ * 	delete_dnodeint_at_index
+ * Description:
+ * 	Deletes a node from at a given index.
+ * NTK:
+ * 	@head: A pointer to the head of the dlistint_t.
+ * 	@index: The index of the node to delete.
+ * Return: *  1.
+ *         * -1.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+
+	free(tmp);
+	return (1);
+}
